f1fd1ad79d04c4516b6920d945e5bb3395d41003
raincloudy507 <mailparthapoddar@yahoo.com>
2026-04-21 23:12:09 -0400

    Comment out unused state and functions in Footer.js
    Comment out unused state and functions in Footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ import './Footer.css';
 
 
 const Footer = () => {
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	// const [isMenuOpen, setIsMenuOpen] = useState(false);
 	// 1. Add state to track the active link
 	// const [activeLink, setActiveLink] = useState('Home');
 
 	// const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
 	// 2. Function to handle link clicks
-	{/* const handleNavLinkClick = (text) => {
+{/*    const handleNavLinkClick = (text) => {
 		setActiveLink(text);
 		setIsMenuOpen(false); // Close mobile menu when a link is clicked
 	}; */}
@@ -198,7 +198,7 @@ const Footer = () => {
 	);
 };
 
-{/*const NavItem = ({ href, text, isActive, onClick }) => (
+{/* const NavItem = ({ href, text, isActive, onClick }) => (
 	<li className="navigation-item">
 		<a
 			href={href}
