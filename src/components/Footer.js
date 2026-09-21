0fd06cdacc9459351c2c1ae851c19b99faddd9da
raincloudy507 <mailparthapoddar@yahoo.com>
2026-09-21 10:39:57 -0400

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
-// import React, {useState} from 'react';
-// import React from 'react';
+import React, {useState} from 'react';
 import './Footer.css';
 // import PrivacyPolicy from './Privacy_policy';
 // import TermsOfService from './Terms_of_service';
